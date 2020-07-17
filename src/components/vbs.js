706e0f10b281da078b5a6b97a4030905108c7269
Rob Martin <rob.martin.5758@gmail.com>
2020-07-17 14:46:10 -0400

    fix internal link to vbs

--- src/components/vbs.js
+++ src/components/vbs.js
@@ -5,33 +5,34 @@ import { FaPhone } from "react-icons/fa"
 
 const Vbs = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <a name="vbs"></a>
+      <div className={styles.container}>
+        <div className={`${styles.image} ${styles.children}`}>
+          <img src={image} alt="Compassion VBS logo"></img>
+        </div>
 
-      <div className={`${styles.image} ${styles.children}`}>
-        <img src={image} alt="Compassion VBS logo"></img>
-      </div>
-
-      <div className={styles.children}>
-        <h2>At-Home Virtual Vacation Bible School</h2>
-        <p className={styles.text}>July 27 – 31</p>
-        <p className={styles.text}>
-          Children will discover how Compassion helps us: welcome others, be
-          brave, love ourselves, be present with each other, and gives us a
-          fresh start.
-        </p>
-        <p className={styles.text}>
-          Click button below for more information and registration or call
-          <a className={styles.link} href="tel:1-276-224-2811">
-            <FaPhone /> 276-224-2811
-          </a>
-        </p>
-        <div>
-          <a href="https://docs.google.com/forms/d/e/1FAIpQLSeCCYD5jZCh4O5DRUeBPgUsKKEJtl9EhbtEbI9ZGSOYdAtWwA/viewform">
-            <button className={styles.button}>
-              Information and Registration
-            </button>
-          </a>
+        <div className={styles.children}>
+          <h2>At-Home Virtual Vacation Bible School</h2>
+          <p className={styles.text}>July 27 – 31</p>
+          <p className={styles.text}>
+            Children will discover how Compassion helps us: welcome others, be
+            brave, love ourselves, be present with each other, and gives us a
+            fresh start.
+          </p>
+          <p className={styles.text}>
+            Click button below for more information and registration or call
+            <a className={styles.link} href="tel:1-276-224-2811">
+              <FaPhone /> 276-224-2811
+            </a>
+          </p>
+          <div>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSeCCYD5jZCh4O5DRUeBPgUsKKEJtl9EhbtEbI9ZGSOYdAtWwA/viewform">
+              <button className={styles.button}>
+                Information and Registration
+              </button>
+            </a>
+          </div>
         </div>
       </div>
     </div>
