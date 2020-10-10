b98364c258175af7b3d6fd51994b08059da28b9b
Rob Martin <rob.martin.5758@gmail.com>
2020-10-09 20:06:57 -0400

    oct 11 order of service added

--- src/components/services.js
+++ src/components/services.js
@@ -48,9 +48,9 @@ const Services = () => {
           <form>
             <button
               className={`${styles.button} ${styles.button_wide}`}
-              formAction="https://1drv.ms/b/s!AiflQFCf7v9pqx9CJYEx_QQ2uy-d?e=7vLYbx"
+              formAction="https://1drv.ms/b/s!AiflQFCf7v9pqyjBZYZDT_iAWqEl?e=7qpHu3"
             >
-              Order of Service for Sunday Oct 4, 2020
+              Order of Service for Sunday Oct 11, 2020
             </button>
           </form>
         </main>
