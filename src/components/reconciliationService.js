1c7df6ce2bd84c59d650503d5620f9e5ba299155
Rob Martin <rob.martin.5758@gmail.com>
2020-11-01 19:51:43 -0500

    add phone numbers for zoom

--- src/components/reconciliationService.js
+++ src/components/reconciliationService.js
@@ -16,12 +16,20 @@ const ReconciliationService = () => {
         <h2>
           Church of The Epiphany's Virtual Service of Healing and Reconciliation
         </h2>
-        <h2>Wednesday, November 4, 2020 | 5:30pm</h2>
+        <h2>Wednesday, November 4 | 5:30pm to 7 pm</h2>
         <h2>
           <a href="https://us02web.zoom.us/j/82435350638">
             <button className={styles.button}>Join the Zoom Meeting</button>
           </a>
         </h2>
+        <h3>
+          Or call one these numbers and use Meeting ID: 824 3535 0638
+          <ul>
+            <li>(312)626-6799</li>
+            <li>(646)558-8656</li>
+            <li>(301)715-8592</li>
+          </ul>
+        </h3>
       </div>
       {/* <div className={`${styles.image}`}>
           <img src={image} alt="Compassion VBS logo"></img>
