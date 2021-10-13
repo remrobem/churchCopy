62ab943868dfbac0d7e480cad9547cfc59a0932b
Rob Martin <rob.martin.5758@gmail.com>
2021-10-13 12:38:35 -0400

    add progress bar

---
+++ src/components/estimateProgressBar.js
@@ -0,0 +1,26 @@
+import React from "react"
+import styles from "./estimateProgressBar.module.css"
+
+const EstimateProgressBar = () => {
+  const goal = 120000
+  const current = 41000
+  const progress = Math.round((current / goal) * 100)
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.text}>Goal: ${goal.toLocaleString('en', {useGrouping:true})}</h2>
+      <div className={styles.progressbarContainer}>
+        <div
+          className={styles.progressbarComplete}
+          style={{ width: `${progress}%` }}
+        >
+          <div className={styles.progressbarLiquid}></div>
+        </div>
+        <span className={styles.progress}>{progress}%</span>
+      </div>
+      <h2 className={styles.text}>Current: ${current.toLocaleString('en', {useGrouping:true})}</h2>
+
+    </div>
+  )
+}
+
+export default EstimateProgressBar
