bdadf72c73e829878a93fb9314aaeca17c1b8fb7
Rob Martin <rob.martin.5758@gmail.com>
2022-01-01 17:12:40 -0500

    updated announcements

--- src/components/announcements.js
+++ src/components/announcements.js
@@ -6,54 +6,6 @@ import { FaEnvelope } from "react-icons/fa"
 const Announcements = () => {
   return (
     <div className={styles.container} id="announcements">
-      <div className={styles.announcement} id="20211114">
-        <h1>Upcoming Services Schedule</h1>
-        <br></br>
-        <div className={styles.text}>
-          <h2 className={styles.serviceHeading}>Longest Night Service</h2>
-          <h2 className={styles.serviceSubHeading}>December 21 Tuesday</h2>
-          <h2 className={styles.serviceSubHeading}> 5:30 pm.</h2>
-          <p>
-            On the Winter Solstice, the longest night of the year, we offer this
-            service especially for those who may not feel “merry and bright”
-            this time of year. Whatever the reason, this service acknowledges
-            that loss, grief, depression and other realities exist even as we
-            await the birth of Jesus. All are welcome to this quiet service with
-            no music or Holy Communion. Please invite friends and family to join
-            us.
-          </p>
-
-          <h2 className={styles.serviceHeading}>Christmas Eve Service</h2>
-          <h2 className={styles.serviceSubHeading}>December 24 Friday</h2>
-          <h2 className={styles.serviceSubHeading}>5:00 pm</h2>
-          <p>
-            All are welcome. It will be a family service and, as always,
-            children are welcome and encouraged to participate.
-          </p>
-          <h2 className={styles.serviceHeading}>Christmas Day Service</h2>
-          <h2 className={styles.serviceSubHeading}>December 25 Saturday</h2>
-          <h2 className={styles.serviceSubHeading}>10:00 am</h2>
-          <p>A informal service for everyone.</p>
-
-          <h2 className={styles.serviceHeading}>
-            First Sunday after Christmas
-          </h2>
-          <h2 className={styles.serviceSubHeading}>December 26 Sunday</h2>
-          <h2 className={styles.serviceSubHeading}>10:00 am</h2>
-          <p>
-            We will sing Christmas carols and celebrate Holy Communion. We hope
-            you will join us and bring your friends and family.
-          </p>
-
-          <h2 className={styles.serviceHeading}>Epiphany</h2>
-          <h2 className={styles.serviceSubHeading}>January 6 Thursday</h2>
-          <p>
-            There will be a celebration of Epiphany, our Feast Day, on January
-            6. More information will be available soon.
-          </p>
-          <br></br>
-        </div>
-      </div>
 
       <div className={styles.announcement} id="20211114">
         <h1>Office Hours</h1>
@@ -68,36 +20,13 @@ const Announcements = () => {
       </div>
 
       <div className={styles.announcement} id="20211114">
-        <h1>Sunday services are being held indoors at 10 am</h1>
+        <h1>In-person Sunday services are suspended.</h1>
+        {/* <h1>Sunday services are being held indoors at 10 am</h1> */}
         <div className={styles.text}>
           <p>
             See <Link to="/#indoor">Sunday Services</Link> below for more
             details.
           </p>
-          <br></br>
-        </div>
-      </div>
-      <div className={styles.announcement}>
-        <h1>Feast of the Baptism of Jesus</h1>
-        <div className={styles.text}>
-          <p>Sunday, January 9, is the Feast of the Baptism of Jesus.</p>
-          <p>
-            If you or a family member wish to receive the Sacrament of Baptism
-            on that day, please email Mother Linda or Carol by December 31.
-          </p>
-          <p>
-            {" "}
-            <a className={styles.link} href="mailto:lindaynye@epiphanyeden.org">
-              <FaEnvelope /> lindanye@epiphanyeden.org
-            </a>
-          </p>
-
-          <p>
-            <a className={styles.link} href="mailto:church@epiphanyeden.org">
-              <FaEnvelope /> church@epiphanyeden.org
-            </a>
-          </p>
-          <br></br>
           <br></br>
         </div>
       </div>
