7f00af97e97848970ab279ff117777a665952de0
Rob Martin <rob.martin.5758@gmail.com>
2022-01-30 09:46:14 -0500

    add jan 30 service

--- src/components/services.js
+++ src/components/services.js
@@ -64,8 +64,8 @@ const Services = () => {
             <iframe
               // width="100vw"
               // height="100vh"
-              src="https://www.youtube.com/embed/y8I-b40m7pk"
-              title="Sunday January 23, 2022"
+              src="https://www.youtube.com/embed/EJR6H8YGFZo"
+              title="Sunday January 30, 2022"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowfullscreen
