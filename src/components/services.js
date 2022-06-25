be3dcb1f900d444c813325f58fcf71413410c4f7
Rob Martin <rob.martin.5758@gmail.com>
2022-06-25 10:57:21 -0400

    sync services from cms

--- src/components/services.js
+++ src/components/services.js
@@ -83,8 +83,8 @@ const Services = () => {
             <iframe
               width="300"
               height="300"
-              src="https://www.youtube.com/embed/NBWqMEYFe5g"
-              title="June 12, 2022"
+              src="https://www.youtube.com/embed/K-x2BSegVj4"
+              title="June 19, 2022"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
