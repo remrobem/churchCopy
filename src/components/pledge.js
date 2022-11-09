b52878308393e911828c783c9daad88a13d31129
Rob Martin <rob.martin.5758@gmail.com>
2022-11-09 08:26:47 -0500

    remove pledge components

--- src/components/pledge.js
+++ src/components/pledge.js
@@ -3,8 +3,8 @@ import * as styles from "./pledge.module.css"
 
 const Pledge = () => {
   return (
-    <div className={styles.container} id="epiphany_pledge">
-      <h2 className={styles.title}>Estimated Giving</h2>
+    <div className={styles.container} id="pledge">
+      <h2 className={styles.title}>Pledging for 2023</h2>
       <blockquote className={styles.quote}>
         <p className={styles.text}>
           “For nothing will be impossible with God.”
@@ -12,12 +12,12 @@ const Pledge = () => {
         <footer>Luke 1:37</footer>
       </blockquote>
       <p className={styles.text}>
-        You can provide your estimate of giving by sending an email to giving@epiphanyeden.org
+        You can provide your pledge amount by sending an email to pledge@epiphanyeden.org
       </p>
       <p className={styles.text}>
-        or by using the Estimated Giving button below to open an email for you.
+        or by using the Pledge button below to open an email for you.
       </p>
-      <p className={styles.text}>Your estimated giving amount can also be mailed to:</p>
+      <p className={styles.text}>Your pledge can also be mailed to:</p>
       <p className={styles.text}>Church of the Epiphany</p>
       <p className={styles.text}>538 Henry St</p>
       <p className={styles.text}>Eden NC 27288</p>
@@ -28,12 +28,12 @@ const Pledge = () => {
             className={styles.button}
             a
             href="#mailgo"
-            data-address="giving"
+            data-address="pledge"
             data-domain="epiphanyeden.org"
-            data-subject="My 2021 Estimate of Giving"
-            data-body={"Name: \nEstimate of Giving amount for 2021: "}
+            data-subject="My Pledge for 2023"
+            data-body={"Name: \nPledge amount for 2023: "}
           >
-            Estimated Giving Email
+            Pledge Email
           </button>
         </form>
       </div>
