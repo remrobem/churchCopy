12f34a1bf790b1f8791f3b222f80adfe0e0a275f
Rob Martin <rob.martin.5758@gmail.com>
2022-12-17 19:06:04 -0500

    initial horo date changes

--- src/components/homelessShelterMeals2023.js
+++ src/components/homelessShelterMeals2023.js
@@ -1,35 +1,40 @@
 import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
-import * as styles from "./morningPrayer.module.css"
+import * as styles from "./homelessShelterMeals2023.module.css"
 
-const MorningPrayer = () => {
+const HomelessShelterMeals2023 = () => {
   return (
     <main>
       <section className={styles.imageContainer} id="morningprayer">
         <StaticImage
-          src="../..//content/assets/prayerBook.jpg"
-          alt="Scoop For School"
+          src="../..//content/assets/horo-header.jpg"
+          alt="HORO Logo"
           loading="eager"
+          backgroundColor="blueviolet"
         />
       </section>
       <section className={styles.container}>
         <p className={styles.text}>
-          Beginning September 12 at 6:30 am, we will have Morning Prayer on Zoom
-          on Monday Mornings.
+          We have our dates for bringing meals to the Homeless Shelter. The dates are for dinner and breakfast the following morning.
         </p>
         <p className={styles.text}>
-          You are welcome to come as you are, the Lord doesn't care if you show
-          up in PJs.
+          <ul>
+            <l1>Jan 19 Thur</l1>
+            <l1>Jan 26 Thur</l1>
+            <l1>Feb 8 Wed</l1>
+            <l1>Feb 22 Wed</l1>
+            <l1>Mar 23 Thur</l1>
+            <l1>Mar 30 Thur</l1>
+            <l1>Apr 6 Thur</l1>
+            <l1>Apr 20 Thur</l1>
+          </ul>
         </p>
         <p className={styles.text}>See you there!!</p>
-        <p className={styles.text}>If you need a Book of Common Prayer, email outreach@epiphanyeden.org</p>
         <p className={styles.text}>
-          <a href="https://us02web.zoom.us/j/9599583196?pwd=Zm1JS3Q1S2VYVVFrODNKSTIxaytsdz0">
-            <button className={styles.button}>Join Morning Prayer</button>
-          </a>
+         Please email <a href="mailto:outreach@epiphanyeden.org">outreach@epiphanyeden.org</a> if you would like to assist on one of these dates.
         </p>
       </section>
     </main>
   )
 }
-export default MorningPrayer
+export default HomelessShelterMeals2023
