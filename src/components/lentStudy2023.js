e14a8e90fd6b3e9bc14625764263215a0f65ca64
Rob Martin <rob.martin.5758@gmail.com>
2023-02-28 13:32:26 -0500

    change lentend study to new schedule

--- src/components/lentStudy2023.js
+++ src/components/lentStudy2023.js
@@ -1,20 +1,44 @@
 import React from "react"
+import "@fontsource/aboreto"
+import "@fontsource/glory"
+
 import * as styles from "./lentStudy2023.module.css"
-import { StaticImage } from "gatsby-plugin-image"
+
 const LentStudy2023 = () => {
   return (
-    <div className={styles.container} id="lent">
-      {/* <h1 className={styles.text}>Ashes.</h1>
-      <h1 className={styles.text}>Wednesday.</h1>
-      <h1 className={styles.text}>Noon and 6 pm.</h1>
-      <h1 className={styles.text}>The 6 pm service as at LUMC</h1> */}
-      <StaticImage
-        src="../../content/assets/lentenStudy2023.png"
-        alt="Lenten Study info"
-        loading="eager"
-        object-fit="cover"
-        className={styles.image}
-      />
+    <div>
+      <div className={styles.container} id="lent">
+        <h1 className={styles.heading}>Lenten Study Series</h1>
+        <p className={styles.text}>
+          Epiphany is having a Lenten study series with St. Thomas, Reidsville.
+        </p>
+        <p className={styles.text}>
+          We will begin with a light soup supper at 5:00 and the program will
+          begin at 6:00. Soup, bread, salad and drinks will be provided.{" "}
+        </p>{" "}
+        <p className={styles.text}>
+          We will alternate between Epiphany and St. Thomas for our meetings
+          each week.{" "}
+        </p>{" "}
+        <p className={styles.text}>
+          We’ll try a hybrid meeting starting at 6:00 and see if that will work
+          for future weeks.
+        </p>
+        <p className={styles.text}>
+          The full schedule and zoom link are below.
+        </p>{" "}
+        <p className={styles.text}>
+          <a href="https://us02web.zoom.us/j/89237049357">Zoom Link</a>
+        </p>
+        <p className={styles.dates}>February 28 - Epiphany</p>
+        <p className={styles.dates}>March 7 - St. Thomas</p>
+        <p className={styles.dates}>March 14 - Epiphany</p>
+        <p className={styles.dates}>March 21 - St. Thomas</p>
+        <p className={styles.dates}>March 28 - Epiphany</p>
+        <p className={styles.dates}>
+          March 5 (Wednesday) - Tenebrae service - Epiphany (no dinner)
+        </p>
+      </div>
     </div>
   )
 }
