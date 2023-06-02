13d399d96b5a8c41eb71fd494c4860854e28ced8
Rob Martin <rob.martin.5758@gmail.com>
2023-06-02 13:24:17 -0400

    service in park

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -4,24 +4,22 @@ import * as styles from "./landingPage.module.css"
 const LandingPage = () => {
   return (
     <div className={styles.container}>
-      
       <div className={styles.messageTop}>
-        {/* <div className={styles.message}>
-          <h1>ALL ARE WELCOME</h1>
-        </div> */}
-        <div className={styles.message}>
-          {/* <h1>
+        <div>
+          <h1 className={styles.message}>ALL ARE WELCOME</h1>
+        </div>
+        <div>
+          <h1 className={styles.message}>
             We believe in a loving, liberating, and life-giving God: Father,
             Son, and Holy Spirit.
-          </h1> */}
-          <h1 className={styles.message}>
-            Pray for Ukraine
           </h1>
+          {/* <h1 className={styles.message}>
+            Pray for Ukraine
+          </h1> */}
         </div>
       </div>
     </div>
   )
 }
-
 
 export default LandingPage
