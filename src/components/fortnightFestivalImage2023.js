0547285baf8cab9bae456af3f418117fad928a91
Rob Martin <rob.martin.5758@gmail.com>
2023-06-21 11:20:38 -0400

    add mp to freedom fortnight

--- src/components/fortnightFestivalImage2023.js
+++ src/components/fortnightFestivalImage2023.js
@@ -18,6 +18,9 @@ const FortnightFestivalImage2023 = ({ alt }) => (
       alt={alt}
       className={styles.image}
     />
+    <div className={styles.map}>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3207.3336054541933!2d-79.77271472486018!3d36.4978083846882!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8852eaebc06045b9%3A0xbca145a6c30ef9dd!2sBridge%20Street%20Recreation%20Center!5e0!3m2!1sen!2sus!4v1687359173461!5m2!1sen!2sus" width="400" height="300" style={{ border:0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
   </div>
 );
 
