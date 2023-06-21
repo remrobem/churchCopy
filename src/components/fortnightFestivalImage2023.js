c93f04354c5f714dd6c537d126a3f91bb3d32a48
Rob Martin <rob.martin.5758@gmail.com>
2023-06-21 11:05:28 -0400

    update freedom fortnight

--- src/components/fortnightFestivalImage2023.js
+++ src/components/fortnightFestivalImage2023.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 import * as styles from './fortnightFestival2023.module.css';
-// import MyImage from '../../content/assets/freedomFortnight2023.jpg';
 
 const FortnightFestivalImage2023 = ({ alt }) => (
   <div className={styles.container}>
-  <StaticImage
-    src="../../content/assets/freedomFortnight2023.jpg"
-    alt={alt}
-    className={styles.image}
-  />
-  <p className={styles.text}>It is Free.  {" "}
-  <a href="https://bit.ly/freedomfortnight">Registration </a> not required but it is helpful.</p>
-</div> 
+    <h1 className={styles.header}>
+      The Freedom Fortnight Festival has been moved to the Bridge St. Rec Center
+    </h1>
+
+    <p className={styles.text}>Same date and time: Saturday, June 24 from noon until 4 pm.</p>
+    <p className={styles.text}>It is Free.</p>
+    <p>
+      <a href="https://bit.ly/freedomfortnight">Registration </a> is not required but it is helpful.
+    </p>
+    <StaticImage
+      src="../../content/assets/freedomFortnight2023.jpg"
+      alt={alt}
+      className={styles.image}
+    />
+  </div>
 );
 
 export default FortnightFestivalImage2023;
